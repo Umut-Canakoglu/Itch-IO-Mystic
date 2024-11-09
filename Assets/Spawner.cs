fcb9db13ec37273e6a3bc9c62a7d8198bfb26d8c
Umut-Canakoglu <148465244+Umut-Canakoglu@users.noreply.github.com>
2024-11-09 15:24:05 +0300

    spawner made better

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] items;
+    private float count = 0;
     void Start()
     {
-       foreach (GameObject item in items) 
-       {
-            float xPosition = Random.Range(-9f, 9f);
+        float parts = 18/items.Length;
+        foreach (GameObject item in items) 
+        {
+            float xPosition = Random.Range((count*parts)-9f, ((count+1)*parts)-9f);
             float yPosition = Random.Range(-5f, 5f);
             Vector2 positionObject = new Vector2(xPosition, yPosition);
             Instantiate(item, positionObject, Quaternion.identity);
-       }
+            count ++;
+        }
     }
 }
